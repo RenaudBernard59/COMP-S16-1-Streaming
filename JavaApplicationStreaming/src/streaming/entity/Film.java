dc30fcc89e996812ba53ec209d95426c677a931c
Renaud BERNARD <renaud.bernard@p59production.com>
2018-04-10 12:11:36 +0200

    Continue linking
    Continue the link @OneToMany.

--- JavaApplicationStreaming/src/streaming/entity/Film.java
+++ JavaApplicationStreaming/src/streaming/entity/Film.java
@@ -6,12 +6,16 @@
 package streaming.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 
@@ -31,8 +35,8 @@ public class Film implements Serializable {
     private String titre;   
     @Column(name = "Description", length = 4000)
     private String synopsis;
-    
-  
+    @OneToMany(mappedBy = "film")
+    private List<Lien> liens = new ArrayList<>();
     
     public String getTitre() {
         return titre;
